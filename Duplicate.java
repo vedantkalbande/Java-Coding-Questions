4ce09aaf9f8f67fea347a1444c0b0f0e19a6bfb2
vedantkalbande <131011500+vedantkalbande@users.noreply.github.com>
2024-04-02 22:34:57 +0530

    Add files via upload

---
+++ Duplicate.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Duplicate {
+
+    public class RemoveDuplicatesFromArray {
+    public static int[] removeDuplicates(int[] arr) {
+        Set<Integer> set = new LinkedHashSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+        int[] result = new int[set.size()];
+        int index = 0;
+        for (int num : set) {
+            result[index++] = num;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+        int[] result = removeDuplicates(arr);
+        System.out.println("Array after removing duplicates:");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
+    
+}
